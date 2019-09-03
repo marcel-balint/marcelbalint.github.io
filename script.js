c63f28dbd82a21c7e7285dff14978154ec6cb7ef
Marcel Balint <blank_blankness@yahoo.com>
2019-09-03 09:29:35 -0400

    Added a scroll effect

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
