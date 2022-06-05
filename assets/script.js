98ebef4b0a4201c9984c021d697e4678135ba73f
Marcel Balint <blank_blankness@yahoo.com>
2022-06-05 14:30:18 -0400

    Added scroll effect

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,34 @@
-document.getElementById('currentYear').innerHTML = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+const topImage = document.getElementById("top-img");
 
-$(document).ready(function () {
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
-
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-
-                window.location.hash = hash;
-            });
-        } // End if
-    });
+window.addEventListener("scroll", () => {
+  decreaseOpacity();
 });
 
+function decreaseOpacity() {
+  topImage.style.opacity = 1 - window.pageYOffset / 900;
+  topImage.style.backgroundSize = 150 - window.pageYOffset / 12 + "%";
+}
 
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
